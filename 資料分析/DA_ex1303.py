2e0cb2792f8a81f5e520d5274003bb5d020e8dcc
Maple <73592189+Mapleyoyo@users.noreply.github.com>
2022-03-04 16:47:18 +0800

    Add files via upload

---
+++ 資料分析/DA_ex1303.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+arr = np.array([1,2,3,4,5])
+arr0 = np.array(40)
+print(arr0)
+
+arr1 = np.array([1,2,3,4,5])
+print(arr1)
+
+arr2 = np.array([[1,2,3],[4,5,6]])
+print(arr2)
+
+arr = np.array([3,2,0,1])
+print(np.sort(arr))
+
+arr1 = np.array([[3,2,4],[5,0,1]])
+print(np.sort(arr1))
+
+arr2 = np.array(['banana', 'cherry', 'apple'])
+print(np.sort(arr2))
+
+arr3 = np.array([True, False, True])
+print(np.sort(arr3))
