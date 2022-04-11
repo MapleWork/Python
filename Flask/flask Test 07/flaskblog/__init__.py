080fa24f622325bc7f240209bb25fe7469aed205
Ru <73592189+Mapleyoyo@users.noreply.github.com>
2022-04-11 19:59:21 +0800

    Add files via upload

---
+++ Flask/flask Test 07/flaskblog/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '91bd0225b2d172ec55135a001ef77a3c'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from flaskblog import routes
